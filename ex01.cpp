048281fe3d4629406ab803f84f28a1b370af2289
Lodka234 <spitfire1@op.pl>
2025-11-03 10:08:22 +0000

    zadanie 2

--- ex01.cpp
+++ ex01.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 using namespace std;
+sum_array(int* a, int size){
+    int suma = 0;
+    for(int i = 0; i< size; i++){
+        suma += *(a+1);
+    }
+    return suma;
+}
 int main(){
-    int x = 69;
-    int* p = &x;
-    cout << p << " " << *p << endl;
-    *p =67;
-    cout << *p << endl;
+    int tab[5]= {8,9,7,6,3}
+    cout << sum_array(&tab[0], 5) << endl;
     return 0;
 }
